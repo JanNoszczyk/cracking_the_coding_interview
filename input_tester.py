f19066e47703a5552b9ea4738c4336a690bfebf1
Jan Noszczyk <jannoszczyk@Jans-MacBook-Pro.local>
2018-11-16 10:06:32 +0100

    Second guy

---
+++ input_tester.py
@@ -0,0 +1,8 @@
+from is_unique import is_unique
+
+def main():
+    input_string = input('Enter your string:')
+    print(is_unique(input_string))
+
+if __name__ == "__main__":
+    main()
---
+++ is_unique.py
@@ -0,0 +1,11 @@
+def is_unique(input_string):
+    unique_flag = True
+    encountered_characters = []
+
+    for char in input_string:
+        if char in encountered_characters:
+            unique_flag = False
+        else:
+            encountered_characters.append(char)
+
+    return unique_flag
