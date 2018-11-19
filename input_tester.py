dfaa0eddad42b3191e4b114adc338003e0334844
Jan Noszczyk <jannoszczyk@jans-mbp.speedport_w_724v_typ_a_05011603_06_001>
2018-11-19 23:36:34 +0100

    Hey boi

--- input_tester.py
+++ input_tester.py
@@ -1,8 +1,10 @@
 from is_unique import is_unique
+from check_permutation import check_permutation
 
 def main():
-    input_string = input('Enter your string:')
-    print(is_unique(input_string))
+    input_string1 = input('Enter your 1st string:')
+    input_string2 = input('Enter your 2nd string:')
+    print(check_permutation(input_string1, input_string2))
 
 if __name__ == "__main__":
     main()
